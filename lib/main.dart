1258db4f6668bc3a75da334f45f1f58afd4b9891
hasanozz <hasanozz91@gmail.com>
2024-11-18 14:05:24 +0300

    class ismi fitpill yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class  extends StatelessWidget {
+class Fitpill extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
